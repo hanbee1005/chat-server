1b0349f8b93ae5fa9daea1ac601e11365bb02ada
hanbee <sonhanbi1002@gmail.com>
2023-11-24 17:18:09 +0900

    feat: 타입 지정 생략

--- src/config/data-source.config.ts
+++ src/config/data-source.config.ts
@@ -1,15 +1,15 @@
-import {DataSource, DataSourceOptions} from "typeorm";
-import {Member} from "@/entity/member.entity";
-import { Chatroom } from "@/entity/chatroom.entity";
+import { DataSource } from 'typeorm';
+import { Member } from '@/entity/member.entity';
+import { Chatroom } from '@/entity/chatroom.entity';
 
 export const AppDataSource = new DataSource({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '1234',
-    database: 'chat',
-    synchronize: false,
-    logging: true,
-    entities: [Member, Chatroom]
-} as DataSourceOptions);
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'test123',
+  database: 'chat',
+  synchronize: false,
+  logging: true,
+  entities: ['src/entity/*.ts'],
+});
